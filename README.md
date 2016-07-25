0d0743727818f2c7c2e1c47fa3a90637913593ef
Oleg Frolov <frolololeg@gmail.com>
2016-07-25 14:05:44 +0300

    Added logo

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# Paster — ease paste text to text layers
+![Paster](/paster-logo.png)
+# Paster — multi pasting for sketch text layers.
 
 ## How it works
-![Paster](/sparkline-example.png)
+
 1. Select some text and press cmd+c;
 2. Select text layer in your artboart;
 3. Press cmd+v;
 4. PROFIT
 
 ## How install the plugin
-1. [Download the zip file with the Texter](https://github.com/Volorf/Texter/archive/master.zip).
+1. [Download the zip file with the Texter](https://github.com/Volorf/Paster/archive/master.zip).
 2. Copy the contents to the Plugin Folder (Plugin > Manage Plugins... > [Show Plugins Folder](http://frolovoleg.ru/images/sketch-plugin-folder.png)).
--- paster.js
+++ paster.js
@@ -1,32 +1,37 @@
 var doc = context.document;
 
+// Get selected elements from your artboard
 var selection = context.selection;
 
+// Define array length of elements in your artboart
 var arrayLength = selection.length;
 
-log(arrayLength)
-
+// Get data from your pasteboard
 var pasteBoard = NSPasteboard.generalPasteboard();
 
+// Turn data in the string type
 var stringFromPasteBoard = [pasteBoard stringForType:NSPasteboardTypeString];
 
+// Set separator type. In the next version (1.1) you can change this.
+var separator = "\n"
+
+// How many we meet separator in yout pasteboard data
+var countOfSring = stringFromPasteBoard.search(separator)
+
+// Create empty array from your pasteboardDara
 var arrayFromStringPasteBoard = []
 
-var separator = ", "
+// Fill array with data from pasteboard
+if (countOfSring <= 0) {
+    log("format your text for right format.")
+} else {
+    log("ok " + countOfSring)
+    arrayFromStringPasteBoard = stringFromPasteBoard.split(separator)
 
-function createLabelsWithCustomData(separator) {
-
-  var countOfSring = stringFromPasteBoard.search(separator)
-
-  if (countOfSring <= 0) {
-      log("format your text for right format.")
-  } else {
-      arrayFromStringPasteBoard = stringFromPasteBoard.split(separator)
-
-  for (var i=0; i<arrayLength; i++) {
-      var layer = selection[i]
-      layer.stringValue = arrayFromStringPasteBoard[i]
-    }
+// Populate pasteboard array with selected elements array
+for (var i=0; i<arrayLength; i++) {
+    var layer = selection[i]
+    layer.stringValue = arrayFromStringPasteBoard[i]
   }
 
 }
